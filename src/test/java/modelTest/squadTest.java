934363623b963f5ca2eeba79f8085db4b27b8fb1
savanahatieno <savanaht01@gmail.com>
2021-04-21 16:20:33 +0300

    tested getsquadname method

--- src/test/java/modelTest/squadTest.java
+++ src/test/java/modelTest/squadTest.java
@@ -1,9 +1,19 @@
 package modelTest;
 
+import models.squad;
 import org.junit.Test;
 import org.junit.Assert;
+import org.junit.jupiter.api.DisplayName;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class squadTest {
 
-
+    @Test
+    @DisplayName("Name should be added")
+    public void testString() {
+        squad newSquad = squad.setUpNewSquad1();
+        System.out.println(newSquad);
+        //assertEquals("Team Avatar", newSquad.getSquadName ());
+    }
 }
