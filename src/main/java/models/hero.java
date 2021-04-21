90661583ad9de1901c889655a75c5ef585989ef0
savanahatieno <savanaht01@gmail.com>
2021-04-21 08:12:48 +0300

    added tests

--- src/main/java/models/hero.java
+++ src/main/java/models/hero.java
@@ -1,4 +1,14 @@
 package models;
 
+import java.util.ArrayList;
+import javax.swing.text.html.HTMLWriter;
+import java.lang.reflect.Array;
+
 public class hero {
+    private String name;
+    private int age;
+    private String power;
+    private String weakness;
+    private int id;
+
 }
--- src/test/java/modelTest/heroTest.java
+++ src/test/java/modelTest/heroTest.java
@@ -1,4 +1,15 @@
 package modelTest;
 
+import org.junit.Test;
+
+
+
+import static org.junit.Assert.*;
+
 public class heroTest {
+    @Test
+    public void newHero_getName_String(){
+        Hero newHero = Hero.setUpNewHero();
+        assertEquals(("Aang", newHero.getName());
+    }
 }
