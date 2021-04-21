4122b2ff67c905907d3dcf297d65499fe620266b
savanahatieno <savanaht01@gmail.com>
2021-04-21 08:28:34 +0300

    created getter methods for hero

--- src/main/java/models/hero.java
+++ src/main/java/models/hero.java
@@ -10,5 +10,25 @@ public class hero {
     private String power;
     private String weakness;
     private int id;
+    private static  ArrayList instances = new ArrayList();
+
+    public hero (String name, Integer age, String power, String weakness,){
+        this.name = name;
+        this.age = age;
+        this.power = power;
+        this.weakness = weakness;
+        this.id = instances.size();
+        instances.add(this);
+    }
+
+    //Return
+
+    public String getName(){
+        return this.name;
+    }
+     //Power
+    public int getAge(){
+        return this.age;
+    }
 
 }
--- src/test/java/modelTest/heroTest.java
+++ src/test/java/modelTest/heroTest.java
@@ -8,8 +8,8 @@ import static org.junit.Assert.*;
 
 public class heroTest {
     @Test
-    public void newHero_getName_String(){
+    public void setUpNewHero_getName_String(){
         Hero newHero = Hero.setUpNewHero();
-        assertEquals(("Aang", newHero.getName());
+        assertEquals(("Aang", newHero.getName);
     }
 }
