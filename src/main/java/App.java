beaa9468b96314b987cd1ade59b5acce5f13ed6b
savanahatieno <savanaht01@gmail.com>
2021-04-21 19:59:19 +0300

    Added imports to java app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,18 @@
+import models.hero;
+import models.squad;
+
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 
 public class App {
     public static void main(String[] args) {
-        get("/hello", (request, response) -> "Hello Friend!");
+
     }
 }
